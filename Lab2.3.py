4fbeab71dde0d50cac09b1845a6e5ef941ae4747
Nek1s <inn282006@gmail.com>
2024-10-18 01:00:10 +0300

    Update Lab2.3.py

--- Lab2.3.py
+++ Lab2.3.py
@@ -1,2 +1,15 @@
-import os
-os.system('echo Hello, world!')
+def is_prime(number):
+    if number < 2:  
+        return False
+    for i in range(2, int(number**0.5) + 1):  
+        if number % i == 0:
+            return False  
+    return True  
+
+
+number = int(input("Введите ваше число: "))
+if is_prime(number):
+    print("Число простое.")
+else:
+    print("Число не является простым.")
+  
